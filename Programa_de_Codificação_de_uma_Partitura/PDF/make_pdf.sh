49f0c271b1429b1ac5e1170170c0c571b68d4d44
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-06-02 16:21:31 -0300

    pequenos ajustes ao script make_pdf.sh do Programa_de_Codificação_de_uma_Partitura

--- Programa_de_Codificação_de_uma_Partitura/PDF/make_pdf.sh
+++ Programa_de_Codificação_de_uma_Partitura/PDF/make_pdf.sh
@@ -2,11 +2,11 @@ echo "Generating lowres pages:"
 for f in ../originais/*.jpg
 do
     echo "Converting $f ..."
-    convert $f -rotate 90 $f.rotacionado.jpg
+    convert $f -resize 2000 $f.lowres.jpg
 done
 
 echo "And now generating the lowres PDF..."
-convert ../originais/*.rotacionado.jpg Programa_de_Codificação_de_uma_Partitura.pdf
-rm ../originais/pagina_*.rotacionado.jpg -f
+convert ../originais/*.lowres.jpg Programa_de_Codificação_de_uma_Partitura.pdf
+rm ../originais/paginas_*.lowres.jpg -f
 
 echo "done."
